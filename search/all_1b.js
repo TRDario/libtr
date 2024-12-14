90fcee5e9453a5ba4939ce614d0826944ed7edf0
TRDario <TRDario@users.noreply.github.com>
2024-12-14 18:28:20 +0000

    deploy: 4c2a59cb338fb3cf19625098314304ed5231d123

--- search/all_1b.js
+++ search/all_1b.js
@@ -3,6 +3,5 @@ var searchData=
   ['_7eaudiosystem_0',['~AudioSystem',['../classtr_1_1AudioSystem.html#ad8ccad8ca1bf17dbe59be2ddb1cea9e9',1,'tr::AudioSystem']]],
   ['_7eframebuffer_1',['~Framebuffer',['../classtr_1_1Framebuffer.html#a2bbb8f8143b9583d94d2b368a7a3c457',1,'tr::Framebuffer']]],
   ['_7ehandle_2',['~Handle',['../classtr_1_1Handle.html#ad4241f53911907ee3ff79f13302c5896',1,'tr::Handle']]],
-  ['_7eticker_3',['~Ticker',['../classtr_1_1Ticker.html#a062f8fe0c7d9487086f17c2291ddd338',1,'tr::Ticker']]],
-  ['_7ewindow_4',['~Window',['../classtr_1_1Window.html#a216f7a0afcdfe22fd1aa8c918daaa87a',1,'tr::Window']]]
+  ['_7eticker_3',['~Ticker',['../classtr_1_1Ticker.html#a062f8fe0c7d9487086f17c2291ddd338',1,'tr::Ticker']]]
 ];
