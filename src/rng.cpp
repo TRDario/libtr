aa8ebe2f85ae2905e21c136bb55e04a221c6efea
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2025-01-17 22:07:01 +0100

    Fix missing namespace in rng.cpp.

--- src/rng.cpp
+++ src/rng.cpp
@@ -1,10 +1,11 @@
 #include "../include/tr/rng.hpp"
+#include <ctime>
 #include <random>
 
-std::uint64_t generateRandomSeed() noexcept
+std::uint64_t tr::generateRandomSeed() noexcept
 {
 	std::random_device rng;
-	return rng() ^ time(nullptr);
+	return rng() ^ std::time(nullptr);
 }
 
 tr::Xorshiftr128p::Xorshiftr128p() noexcept
