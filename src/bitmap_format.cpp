5d41204237df364d8a0bd940abaa0b9569b4af16
Dario Cvitanović <dario.cvitanovic04@gmail.com>
2025-01-22 12:40:01 +0100

    SDL migration: port bitmap_format.

--- src/bitmap_format.cpp
+++ src/bitmap_format.cpp
@@ -1,5 +1,5 @@
 #include "../include/tr/bitmap_format.hpp"
-#include <SDL2/SDL.h>
+#include <SDL3/SDL.h>
 
 tr::BitmapFormat::BitmapFormat(Type type) noexcept
 	: _type{type}
@@ -29,12 +29,12 @@ tr::BitmapFormat::operator Type() const noexcept
 
 const char* tr::BitmapFormat::name() const noexcept
 {
-	return SDL_GetPixelFormatName(SDL_PixelFormatEnum(_type));
+	return SDL_GetPixelFormatName(SDL_PixelFormat(_type));
 }
 
 tr::BitmapFormat::Order tr::BitmapFormat::order() const noexcept
 {
-	int order{SDL_PIXELORDER(SDL_PixelFormatEnum(_type))};
+	int order{SDL_PIXELORDER(SDL_PixelFormat(_type))};
 	if (isPacked()) {
 		order += int(Order::PACKED_NONE);
 	}
@@ -46,35 +46,35 @@ tr::BitmapFormat::Order tr::BitmapFormat::order() const noexcept
 
 tr::BitmapFormat::Layout tr::BitmapFormat::layout() const noexcept
 {
-	return Layout(SDL_PIXELLAYOUT(SDL_PixelFormatEnum(_type)));
+	return Layout(SDL_PIXELLAYOUT(SDL_PixelFormat(_type)));
 }
 
 int tr::BitmapFormat::pixelBits() const noexcept
 {
-	return SDL_BITSPERPIXEL(SDL_PixelFormatEnum(_type));
+	return SDL_BITSPERPIXEL(SDL_PixelFormat(_type));
 }
 
 int tr::BitmapFormat::pixelBytes() const noexcept
 {
-	return SDL_BYTESPERPIXEL(SDL_PixelFormatEnum(_type));
+	return SDL_BYTESPERPIXEL(SDL_PixelFormat(_type));
 }
 
 bool tr::BitmapFormat::isIndexed() const noexcept
 {
-	return SDL_ISPIXELFORMAT_INDEXED(SDL_PixelFormatEnum(_type));
+	return SDL_ISPIXELFORMAT_INDEXED(SDL_PixelFormat(_type));
 }
 
 bool tr::BitmapFormat::isPacked() const noexcept
 {
-	return SDL_ISPIXELFORMAT_PACKED(SDL_PixelFormatEnum(_type));
+	return SDL_ISPIXELFORMAT_PACKED(SDL_PixelFormat(_type));
 }
 
 bool tr::BitmapFormat::isArray() const noexcept
 {
-	return SDL_ISPIXELFORMAT_ARRAY(SDL_PixelFormatEnum(_type));
+	return SDL_ISPIXELFORMAT_ARRAY(SDL_PixelFormat(_type));
 }
 
 bool tr::BitmapFormat::hasAlpha() const noexcept
 {
-	return SDL_ISPIXELFORMAT_ALPHA(SDL_PixelFormatEnum(_type));
+	return SDL_ISPIXELFORMAT_ALPHA(SDL_PixelFormat(_type));
 }
